d2f5989f83f2d6ba620a5230ab70e6cc7634b661
Curtis Bartell <cbartell980@gmail.com>
2021-01-29 14:21:57 -0700

    Add paper footnotes endpoint

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ app.post('/api/v1/footnotes', (request, response) => {
     });
 });
 
+
 // GET a specific paper:
 app.get('/api/v1/papers/:id', (request, response) => {
   database('papers').where('id', request.params.id).select()
@@ -97,6 +98,24 @@ app.get('/api/v1/papers/:id', (request, response) => {
       else {
         response.status(404).json({
           error: `Could not find paper with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+});
+
+// GET footnotes for a specific paper:
+app.get('/api/v1/papers/:id/footnotes', (request, response) => {
+  database('footnotes').where('paper_id', request.params.id).select()
+    .then(footnotes => {
+      if(footnotes.length) {
+        response.status(200).json(footnotes);
+      }
+      else {
+        response.status(404).json({
+          error: `Could not find footnotes with paper_id ${request.params.id}`
         });
       }
     })
